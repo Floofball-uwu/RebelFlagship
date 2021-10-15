ab7f2e321849443991b8408d7f997ca19dbbc7c3
FarmerThanos <59265546+FarmerThanos@users.noreply.github.com>
2021-10-16 00:10:35 +0200

    Update and rename src/example/ExampleJavaMod.java to src/rebelFlagship/RebelFlagship.java

---
+++ src/rebelFlagship/RebelFlagship.java
@@ -0,0 +1,21 @@
+package rebelFlagship;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class RebelFlagship extends Mod{
+
+    public RebelFlagship(){
+        Log.info("ASB");
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading some example content.");
+    }
+}
